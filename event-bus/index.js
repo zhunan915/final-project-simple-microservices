03d428045458a3e8b0b92993fbc1755bf5e36cfc
zhunan915 <zhunan@bu.edu>
2025-05-01 23:58:19 -0700

    update cross

--- event-bus/index.js
+++ event-bus/index.js
@@ -22,13 +22,13 @@ app.post('/events', async (req, res) => {
   try {
     await Promise.all([
       // post service
-      axios.post('http://posts-clusterip-srv:8001/events', event),
+      axios.post('http://backend-posts-service:8001/events', event),
       // comments service
-      axios.post('http://comments-clusterip-srv:8002/events', event),
+      axios.post('http://backend-comments-service:8002/events', event),
       // query service
-      axios.post('http://query-clusterip-srv:8003/events', event),
+      axios.post('http://backend-query-service:8003/events', event),
       // moderation service
-      axios.post('http://mod-clusterip-srv:8004/events', event),
+      axios.post('http://backend-moderation-service:8004/events', event),
 
     ])
     return res.send({}).end()
