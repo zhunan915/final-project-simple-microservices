8b78c80ec15e1576a033ceeb98d47a94bdcee79e
zhunan915 <zhunan@bu.edu>
2025-05-01 23:29:54 -0700

    update cross

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async event => {
     event.preventDefault();
 
-    await axios.post("http://posts.com/post/create", {
+    await axios.post("http://qa.zhunandomain.live/posts/create", {
       title,
     });
 
