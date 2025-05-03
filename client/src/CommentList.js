c369ba955d105fbe89681d2ce041e4e70ba87420
zhunan915 <zhunan@bu.edu>
2025-05-03 12:37:13 -0700

    update qa

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const CommentList = ({ comments }) => {
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const res = await axios.get(`http://qa.zhunandomain.live/post/${postId}/comments`);
+    setComments(res.data || []);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
   const renderedComments = comments.map((comment) => {
     let content;
     switch (comment.status) {
-      case 'pending':
-        content = 'This comment is awaiting moderation!'
+      case "pending":
+        content = "This comment is awaiting moderation!";
         break;
-      case 'rejected':
-        content = 'This comment has been rejected!'
+      case "rejected":
+        content = "This comment has been rejected!";
         break;
-
       default:
-        content = comment.content
+        content = comment.content;
         break;
     }
+
     return <li key={comment.id}>{content}</li>;
   });
 
