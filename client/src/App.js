fbaae3b155836261b5fd658ca2f19b053fbfbc14
zhunan915 <zhunan@bu.edu>
2025-05-07 01:05:11 -0700

    update text

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import PostList from "./PostList";
 const App = () => {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1>Create Post and Hello</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
